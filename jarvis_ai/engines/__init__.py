c77d3b0a80318eeb407f6996854c8fdbf83900fb
kshomahmadov987-lang <kshomahmadov987@gmail.com>
2026-05-23 18:05:00 +0500

    Revert "Update from task 08733755-6a6b-483c-813d-104fa32873f5"

--- jarvis_ai/engines/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Jarvis AI - Engines Module"""
-
-from .audio_engine import AudioEngine
-from .speech_engine import SpeechEngine
-from .command_processor import CommandProcessor
-from .office_controller import OfficeController
-
-__all__ = ['AudioEngine', 'SpeechEngine', 'CommandProcessor', 'OfficeController']
--- jarvis_ai/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Jarvis AI - Utilities Module"""
-
-from .system_monitor import SystemMonitor
-from .weather import WeatherAPI
-
-__all__ = ['SystemMonitor', 'WeatherAPI']
